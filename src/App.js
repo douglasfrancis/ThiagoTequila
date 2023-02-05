4de87eb3d922c49cd1f7611564277f033a7e0a9f
douglasfrancis <dougiefrancis@gmail.com>
2023-02-05 14:10:25 +0000

    Responsive design work

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ window.addEventListener("scroll", function(e){
             <Link to='/retail' className={scrolled? 'nav-item1' :'nav-item'} >Wholesale</Link>
          </div>
 
-        <Link to='/'>
+        <Link to='/' style={{width: '100vw'}}>
          <img id='logo' src={logo} alt='Thiago logo' />
         </Link>
 
