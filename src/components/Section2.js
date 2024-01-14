7b6346d18c6788de16c559b033d40587f289aa58
douglasfrancis <dougiefrancis@gmail.com>
2024-01-14 10:46:12 +0000

    Add coming soon

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -61,7 +61,7 @@ export default function Section2() {
                 >
                 <img  className="unclicked" src={item.img} alt={item.name} />
                 <h2 className='product-info-title'>{item.name}</h2>
-                <button className='product-btn' style={{width:"200px"}}>Coming Soon</button>
+                <button className='product-btn' style={{width:"200px"}}>Out of Stock</button>
                 <p>{item.desc}</p>
                 </div>
               })}
