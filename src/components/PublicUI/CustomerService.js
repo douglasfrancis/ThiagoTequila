c8c61ac422125e5e45ca2e5c4d5edc4ad830015b
douglasfrancis <dougiefrancis@gmail.com>
2024-07-30 15:45:13 +0100

    New customer service request submission

--- src/components/PublicUI/CustomerService.js
+++ src/components/PublicUI/CustomerService.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CustomerService.css'
 import agave from '../../Assets/customer-service.webp'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default function CustomerService() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [msg, setMsg] = useState("")
+  const [sent, setSent] = useState(false)
+  const [sending, setSending] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!name || !email || !msg){
+      alert("Please add all fields")
+    } else {
+      setSending(true)
+      axios.post(`${process.env.REACT_APP_API_URL}/thiago/customer-service-request`, {name, email, msg, resolved: false})
+      .then((res) => {
+        setSent(true)
+      })
+      .catch((e) => {
+        alert("Error sending")
+      })
+      .finally(() => {
+        setSending(false)
+      })
+    }
+  }
+
   return (
     <div style={{display:'flex'}}>
         <img id='customer-service-agave' src={agave} alt='Thiago Tequila customer service'/>
@@ -11,16 +40,31 @@ export default function CustomerService() {
             <h2 id='were-here'>We're here</h2>
             <h3 id='to-help'>to help</h3>
 
-            <p style={{letterSpacing: 2}}>GOT A QUESTION OR NEED SOME ASSISTANCE?</p>
+            {sent ?
+            <div>
+              <p style={{color: '#77FD78'}}>Successfully sent!</p>
+              <p>We've received your message and our customer support team will be in touch with you as soon as possible</p>
+              <p style={{marginBottom: 50}}>In the meantime, feel free to explore our website</p>
+              <Link to={'/products'} id='customer-submit-btn' style={{padding: '10px 15px 5px 15px'}}>Explore Now</Link>
 
-            <p>Fill out the form below and our friendly customer support team will be happy to help!</p>
+            </div>
 
-            <form id='customer-form'>
-                <input placeholder='Your name' className='customer-input' />
-                <input placeholder='Your email' className='customer-input' />
-                <textarea  placeholder='Message' className='customer-input' id='customer-textarea'/>
-                <button id='customer-submit-btn'>Submit</button>
-            </form>
+            :
+            <>
+              <p style={{letterSpacing: 2}}>GOT A QUESTION OR NEED SOME ASSISTANCE?</p>
+
+              <p>Fill out the form below and our friendly customer support team will be happy to help!</p>
+
+              <form id='customer-form'>
+                  <input placeholder='Your name' className='customer-input' value={name} onChange={(e) => setName(e.target.value)}/>
+                  <input placeholder='Your email' className='customer-input' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  <textarea  placeholder='Message' className='customer-input' id='customer-textarea' value={msg} onChange={(e) => setMsg(e.target.value)}/>
+                  <button id='customer-submit-btn' onClick={handleSubmit}>{sending ? 'Sending' : 'Submit'}</button>
+              </form>
+            </>
+            }
+
+            
 
         </div>
 
